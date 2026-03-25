150d6c15683430900c21532434f7524a4be5a440
Eylin <eylin@dataa.org>
2026-03-25 13:08:06 +0100

    Solved lab

---
+++ lab_sql_queries.sql
@@ -0,0 +1,63 @@
+USE sakila;
+SHOW TABLES FROM sakila;
+
+SELECT *
+FROM actor;
+
+SELECT *
+FROM customer;
+
+SELECT *
+FROM film;
+
+SELECT title
+FROM film;
+
+SELECT name AS Language
+FROM language;
+
+SELECT first_name
+FROM staff;
+
+SELECT first_name
+FROM staff;
+
+SELECT DISTINCT release_year
+FROM film;
+
+SELECT COUNT(store_id)
+FROM store;
+
+SELECT COUNT(staff_id)
+FROM staff;
+
+SELECT 
+	COUNT(inventory_id), 
+	COUNT(return_date), 
+	(COUNT(inventory_id)-COUNT(return_date)) AS available_to_rent
+FROM rental;
+
+SELECT DISTINCT last_name
+FROM actor;
+
+SELECT title, length
+FROM film
+ORDER BY length DESC
+LIMIT 10
+;
+
+SELECT first_name
+FROM actor
+WHERE first_name = "SCARLETT"
+;
+
+SELECT title, length
+FROM film
+WHERE title = "ARMAGEDDON" AND length = 100
+;
+
+SELECT title, special_features
+FROM film
+WHERE special_features = "Behind the Scenes"
+;
+
